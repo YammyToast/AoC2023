de704b1862961317262ed64679823b7feb370264
James Hardy <cyanjamesmail@gmail.com>
2023-12-09 03:33:25 +0000

    Day 8-1

---
+++ d8/d8-2.py
@@ -0,0 +1,30 @@
+import re
+
+
+def analyse_file(__filename: str):
+    with open(__filename) as f:
+        lines = f.readlines()
+    instructions = list(lines[0].strip("\n"))
+    raw_destinations = [c.strip("\n").split(" = ") for c in lines[2:]]
+    destinations = {}
+    for dest in raw_destinations:
+        destinations[dest[0]] = tuple(re.sub("[() ]", "", dest[1]).split(","))
+    loc = "AAA"
+    steps = 0
+    while loc != "ZZZ":
+        for instruction in instructions:
+            if loc == "ZZZ":
+                print("YEAH IM HERE")
+                break
+            if instruction == "L":
+                loc = destinations[loc][0]
+            else:
+                loc = destinations[loc][1]
+            # loc = destinations[loc][instruction!="L"]
+            steps += 1
+
+    print(loc, steps)
+
+
+if __name__ == "__main__":
+    print(analyse_file("input.txt"))
